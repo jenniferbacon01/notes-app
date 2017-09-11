4021eca0e0c2020da75f2253a37048aba981dcc2
Elizabeth Chan <elizabeth.schan@outlook.com>
2017-09-11 17:43:42 +0100

    NoteListView

--- note-list-view.js
+++ note-list-view.js
@@ -2,17 +2,30 @@
   function NoteListView(noteList) {
     this.noteList = noteList;
     this.stringpart = "";
-    this.string = "<ul>" + this.stringpart + "</ul>";
+    // this.string = "";
   };
   NoteListView.prototype.view = function() {
-    this.noteList.list.forEach(function(note){ this.stringpart += ("<li><div>" + note.text + "</div></li>")});
-    console.log(this.string);
-    return this.string;
+    // var stringpart = "";
+    this.noteList.list.forEach(function(note){
+      var stringpart = "";
+      stringpart+=("<li><div>" + note.text + "</div></li>");
+    console.log("inside forEach: " + stringpart)
+  });
+    console.log("outside forEach: " + stringpart);
+    return ("<ul>" + stringpart + "</ul>");
   };
+
 
   exports.NoteListView = NoteListView;
 })(this);
-
+//
+// note1 = new Note("hi");
+// note2 = new Note("ok");
+// notelist = new NoteList();
+// notelist.add(note1);
+// notelist.add(note2);
+// notelistview = new NoteListView(notelist);
+// notelistview.view()
 
 // "<ul>
 // <li><div>
